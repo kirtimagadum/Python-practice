7bcb186f2ae75adbd5e788c7f4c6df8793fc97f8
kirtimagadum <kirtimagadum794@gmail.com>
2026-03-06 20:03:11 +0530

    Add files via upload

---
+++ maxoftwo.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter first number :"))
+num2=int(input("Enter second number :"))
+if(num1 < num2):
+    print(num2," is large")
+elif(num1==num2):
+    print(num1 ,num2,"Both are equal")
+else:
+    print(num1,"is large")
