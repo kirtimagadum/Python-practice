afa0a53c82a3695bbf569b2757abf3e0e4f70ca4
kirtimagadum <kirtimagadum794@gmail.com>
2026-03-09 20:21:19 +0530

    Add files via upload

---
+++ largestOfThree.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter 1st number:"))
+num2=int(input("Enter 2nd number:"))
+num3=int(input("Enter 3rd number:"))
+if(num1>num2):
+    print(f"{num1} is larger")
+elif(num2>num3):
+    print(f"{num2} is larger")
+else:
+    print(f"{num3} is larger")
